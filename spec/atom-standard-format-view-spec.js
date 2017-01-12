3a836630d3ff361d2b511ef0e681b0ae2501632d
johannes <johannes.zwirchmayr@students.fh-hagenberg.at>
2017-01-12 04:46:30 +0100

    Initial commit.

---
+++ spec/atom-standard-format-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomStandardFormatView from '../lib/atom-standard-format-view';
+
+describe('AtomStandardFormatView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
