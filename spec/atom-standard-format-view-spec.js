26a733aed2aed17d65436ee54f0bb9006176c721
johannes <johannes.zwirchmayr@students.fh-hagenberg.at>
2017-01-12 04:54:01 +0100

    Cleanup package.

--- spec/atom-standard-format-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomStandardFormatView from '../lib/atom-standard-format-view';
-
-describe('AtomStandardFormatView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
